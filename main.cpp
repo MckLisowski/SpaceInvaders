87e8c1697c5e00de082b61392e0e7816e7b3d235
Maciek <37239354+MckLisowski@users.noreply.github.com>
2021-04-18 01:23:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
 	SystemInit();
 
-	while(1)                                       							// do tej pÍtli program wchodzi po wyjsciu z nowej gry
+	while(1)                                       							// do tej pƒôtli program wchodzi po wyjsciu z nowej gry
 	{
 		vector<RocketPtr> rakiety;
 		vector<BulletPtr> naboje;
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
 					for(unsigned int b=0; b < naboje.size(); b++)
 					{
 						if( (( rakiety[a]->getXpos() + rakiety[a]->getWidth()-3 ) >= naboje[b]->getPosX() )
-								and ( naboje[b]->getPosX() >= (rakiety[a]->getXpos()+3 - naboje[b]->getSize()) )) 							// ( -szer naboju+1)
+								and ( naboje[b]->getPosX() >= (rakiety[a]->getXpos()+3 - naboje[b]->getSize()) )) 								// ( -szer naboju+1)
 						{
 							if( (rakiety[a]->getYpos() + rakiety[a]->getLength()) > naboje[b]->getPosY()
 									and naboje[b]->getPosY() > rakiety[a]->getYpos())
@@ -116,11 +116,11 @@ int main(int argc, char *argv[])
 					}
 				}
 
-				// RYSOWANIE CZO≥GU================================================================================================================================================
-					drawTank(tank->getPosX (), tank->getPosY());	// ruch czo≥gu
+				// RYSOWANIE CZO≈ÇGU================================================================================================================================================
+					drawTank(tank->getPosX (), tank->getPosY());	// ruch czo≈Çgu
 
 
-				// KOLIZJE RAKIETY-CZO£G============================================================================================================================================
+				// KOLIZJE RAKIETY-CZO≈ÅG============================================================================================================================================
 				for(unsigned int r=0; r<rakiety.size(); r++)
 				{
 					short rakietaX=rakiety[r]->getXpos();
@@ -137,10 +137,10 @@ int main(int argc, char *argv[])
 				}
 
 				// NOWE RAKIETY===========================================================================================================================================
-				if(gameIteration%game->getSettings()->getIterations()==0)																						// nowe rakiety raz na 10 iteracji
+				if(gameIteration%game->getSettings()->getIterations()==0)														// nowe rakiet co okre≈õlonƒÖ ilo≈õƒá iteracji
 				{
-					pozycjeRakiet=generatePositions(globalTimer_ms, 24);														// generuje pozycje dla kazdej nowej lini rakiet
-					for(unsigned int i=0; i<pozycjeRakiet.size(); i++)																// tworzenie nowej lini rakiet z pozycjami z wskazPoz
+					pozycjeRakiet=generatePositions(globalTimer_ms, 24);														
+					for(unsigned int i=0; i<pozycjeRakiet.size(); i++)														
 					{
 						rocketSpeed=generateRocketSpeed(globalTimer_ms+i);
 						rakiety.push_back(make_shared<Rocket>(24, 32, pozycjeRakiet[i], 0, game->getSettings()->getRocketLife(), rocketSpeed));
@@ -148,12 +148,12 @@ int main(int argc, char *argv[])
 				}
 
 				// RUCH I RYSOWANIE RAKIET================================================================================================================================
-				for(unsigned int j=0; j<rakiety.size(); j++)																			// wykonuje siÍ tyle razy ile jest wszystkich rakiet
+				for(unsigned int j=0; j<rakiety.size(); j++)																// wykonuje siƒô tyle razy ile jest wszystkich rakiet
 				{
 					draw1bitBitmap(bmrocket,  rakiety[j]->getXpos(),  rakiety[j]->getYpos(),  rakiety[j]->getWidth(), rakiety[j]->getLength(), rakiety[j]->getColour());
-//					drawObject(rakiety[j]->getXpos(), rakiety[j]->getYpos(),  rakiety[j]->getWidth(), rakiety[j]->getLength(), rakiety[j]->getColour());	// rysuje wszystkie rakiety z wektora
-					rakiety[j]->moveRocket();																	//przesuwa kazda rakietÍ o zadanπ wartoúc
-					if(rakiety[j]->getYpos()>(480-rakiety[j]->getLength()) or rakiety[j]->getActualLife()<=0)										//rakiety ktÛre sπ dalej niz zadana wartoúc sπ usuwane
+//					drawObject(rakiety[j]->getXpos(), rakiety[j]->getYpos(),  rakiety[j]->getWidth(), rakiety[j]->getLength(), rakiety[j]->getColour());				// rysuje wszystkie rakiety z wektora
+					rakiety[j]->moveRocket();																	//przesuwa kazda rakietƒô o zadanƒÖ warto≈õc
+					if(rakiety[j]->getYpos()>(480-rakiety[j]->getLength()) or rakiety[j]->getActualLife()<=0)									//rakiety kt√≥re sƒÖ dalej niz zadana warto≈õc sƒÖ usuwane
 					{
 						if(rakiety[j]->getActualLife()>0)
 						{
@@ -162,7 +162,7 @@ int main(int argc, char *argv[])
 							game->getStats()->setTankLife(game->getTank()->getLife());
 						}
 						rakiety.erase(rakiety.begin()+j);
-							j-=1;																				// !!!! zmniejszone j øeby nie by≥o luki w odczycie po usuniÍciu rakiety
+							j-=1;																		// !!!! zmniejszone j ≈ºeby nie by≈Ço luki w odczycie po usuniƒôciu rakiety
 					}
 				}
 
@@ -176,10 +176,10 @@ int main(int argc, char *argv[])
 				{
 					drawObject(naboje[r]->getPosX(), naboje[r]->getPosY(), naboje[r]->getSize(), naboje[r]->getSize(), 0xffcc33);
 					naboje[r]->moveBullet(game->getSettings()->getBulletSpeed());
-					if(naboje[r]->getPosY()<10)																				//rakiety ktÛre sπ dalej niz zadana wartoúc sπ usuwane
+					if(naboje[r]->getPosY()<10)																	
 					{
 						naboje.erase(naboje.begin()+r);
-						r-=1;																							// !!!! zmniejszone j øeby nie by≥o luki w odczycie po usuniÍciu rakiety
+						r-=1;																			
 					}
 				}
 
@@ -204,116 +204,4 @@ int main(int argc, char *argv[])
 		rakiety.clear();
 		naboje.clear();
 	} //while zewn
-}
-
-//===============================================================================================================================================================
-//=============KONIEC MAIN=======================================================================================================================================
-//===============================================================================================================================================================
-
-void* TimerThread(void* arguments)
-{
-    struct timespec destTime;
-    clock_gettime(CLOCK_MONOTONIC,&destTime);
-	while(1)
-	{
-		destTime.tv_nsec+=1000000;
-		if(destTime.tv_nsec>=1E9)
-		{
-			destTime.tv_nsec-=1E9;
-			destTime.tv_sec++;
-		}
-		globalTimer_ms++;
-		if(!(globalTimer_ms%1000)) flash_ligth=0xFF;
-			clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &destTime, NULL);
-	}
-return NULL;
-}
-
-void SystemInit()
-{
-	FrameBufferFD = open("/dev/fb0", O_RDWR);
-	ioctl(FrameBufferFD, FBIOGET_FSCREENINFO, &fix_info);
-	framebuffer = mmap(NULL, fix_info.line_length * 480, PROT_READ | PROT_WRITE,MAP_SHARED, FrameBufferFD, 0);
-	inputEventsFD = open("/dev/input/event0", O_RDONLY | O_NONBLOCK);
-	pthread_create(&tID, NULL, TimerThread, NULL);
-	struct timeval tTime;
-	gettimeofday(&tTime,NULL);
-	startTime_ms=tTime.tv_sec*1000+tTime.tv_usec/1000;
-}
-
-/*void PrintDiagnosticInfo()
-{
-	struct timeval tTime;
-	gettimeofday(&tTime,NULL);
-	long TimeDiff=(tTime.tv_sec*1000+tTime.tv_usec/1000)-startTime_ms;
-	//long actTime=globalTimer_ms;
-	//if((frame_count%20)==0) printf("%i:: %li:%li=>%li KEYS: %i, %i, %i, %i, %x\n",frame_count,actTime,TimeDiff,actTime-TimeDiff,getKey(), Jx, Jy,JRz,JoYAct.ButtonStates);
-}*/
-
-void UpdateIO()
-{
-	struct input_event ev;
-	while (read(inputEventsFD, &ev, sizeof ev) > 0) {
-		if (ev.type == EV_KEY)
-		{
-			switch (ev.value)
-			{
-				case 1:
-					gRawKeys[0] = ev.code;
-					break;
-				case 0:
-					gRawKeys[0] = 0;
-					break;
-				default:
-					break;
-			}
-				switch (ev.code)
-				{
-					case KEY_KP4: JoYAct.AxisX =-127*(ev.value&1); break;
-					case KEY_KP6: JoYAct.AxisX =127*(ev.value&1); break;
-					case KEY_KP8: JoYAct.AxisY =-127*(ev.value&1); break;
-					case KEY_KP2: JoYAct.AxisY =127*(ev.value&1); break;
-					case KEY_RIGHT:
-						if(ev.value == 1) JoYAct.ButtonStates |=BUTTON_2;
-						if(ev.value == 0) JoYAct.ButtonStates &=~BUTTON_2;
-						break;
-					case KEY_LEFT:
-						if(ev.value == 1) JoYAct.ButtonStates |=BUTTON_4;
-						if(ev.value == 0) JoYAct.ButtonStates &=~BUTTON_4;
-						break;
-					case KEY_UP:
-						if(ev.value == 1) JoYAct.ButtonStates |=BUTTON_1;
-						if(ev.value == 0) JoYAct.ButtonStates &=~BUTTON_1;
-						break;
-					case KEY_DOWN:
-						if(ev.value == 1) JoYAct.ButtonStates |=BUTTON_3;
-						if(ev.value == 0) JoYAct.ButtonStates &=~BUTTON_3;
-						break;
-					default:
-						break;
-				}
-		}
-	}
-}
-
-void ClearScreen()
-{
-	int JRz = 0;
-
-	unsigned int bckgmask =getKey()>>2;
-
-	int stage_max= -JRz/20;
-	if (stage_max < 1)		stage_max = 1;
-	if (stage_max > 9)		stage_max = 9;
-
-	if (JoYAct.ButtonStates & BUTTON_SELECT)
-		stage_max = 24;
-
-	stage++;
-	for (int a = stage % stage_max; a < (640 * 480); a += stage_max)
-	{
-		((u_int32_t*) framebuffer)[fix_info.line_length / 4 * (a / 640)
-				+ a % 640] = GRAPH[a];
-		GRAPH[a] =  bckgmask;      //0xffffff  ((GRAPH[a] & 0xfefefe) >> 1) |
-	}
 }
